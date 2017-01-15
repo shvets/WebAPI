9c08455856ae6863b4e386bdced38a393c0f6073
Alexander Shvets <alexander.shvets@gmail.com>
2017-01-15 14:06:46 -0500

    add files

--- Sources/GidOnlineAPI.swift
+++ Sources/GidOnlineAPI.swift
@@ -3,17 +3,11 @@ import SwiftyJSON
 import SwiftSoup
 
 open class GidOnlineAPI: HttpService {
-  public let URL = "http://gidonline.club"
+  public static let URL = "http://gidonline.club"
   let USER_AGENT = "Gid Online User Agent"
 
   let SESSION_URL1 = "http://pandastream.cc/sessions/create_new"
   let SESSION_URL2 = "http://pandastream.cc/sessions/new"
-
-//  public func available() throws -> Elements {
-//    let document = try fetchDocument(URL)
-//
-//    return try document!.select("div[class='container'] div[class='row']")
-//  }
 
   func sessionUrl() -> String {
     return SESSION_URL1
@@ -35,25 +29,21 @@ open class GidOnlineAPI: HttpService {
     return newPath
   }
 
-  public func getGenres(document: Document, type: String="") throws -> [Any] {
+  public func getGenres(_ document: Document, type: String="") throws -> [Any] {
     var data: [Any] = []
 
     let links = try document.select("div[id='catline'] li a")
 
     for link: Element in links.array() {
       let path = try link.attr("href")
-      var name = try link.attr("title")
+      let text = try link.text()
 
-//      let index1 = name.startIndex
-//      let index2 = name.index(name.endIndex, offsetBy: -18)
-//      name = name[index1 ..< index2]
+      let index1 = text.index(after: text.startIndex)
+      let index2 = text.index(before: text.endIndex)
 
-      //    path = link.xpath('@href')[0].text
-//    name = link.xpath('text()')[0].text
-//
-//    list << {"path": path, "name": name[0] + name[1..-1].downcase}
+      let name = text[text.startIndex...text.startIndex] + text[index1...index2].lowercased()
 
-      data.append(["path": path, "name":name])
+      data.append(["path": path, "name": name ])
     }
 
     let family_group = [
@@ -103,22 +93,15 @@ open class GidOnlineAPI: HttpService {
     }
   }
 
-  public func getTopLinks(document: Document) throws -> [Any] {
+  public func getTopLinks(_ document: Document) throws -> [Any] {
     var data: [Any] = []
 
     let links = try document.select("div[id='topls'] a[class='toplink']")
-    //links = document.xpath('//div[@id="topls"]/a[@class="toplink"]')
 
     for link: Element in links.array() {
       let path = try link.attr("href")
-      let name = try link.attr("title")
-      let thumb = URL
-
-//      path = link.xpath('@href')[0].text
-//      name = link.xpath('text()')[0].text
-//      thumb = URL + (link.xpath('img')[0].xpath("@src"))[0]
-//
-//      list << {"path": path, "name": name, "thumb": thumb}
+      let name = try link.text()
+      let thumb = GidOnlineAPI.URL + (try link.select("img").attr("src"))
 
       data.append(["path": path, "name": name, "thumb": thumb])
     }
@@ -126,75 +109,228 @@ open class GidOnlineAPI: HttpService {
     return data
   }
 
-  public func getActors(document: Document, letter: String="") throws -> [Any] {
+  public func getActors(_ document: Document, letter: String="") throws -> [Any] {
     var data: [Any] = []
 
-//    all_list = fix_name(get_category('actors-dropdown', document))
-//
-//    all_list.sort_by! {|item| item[:name]}
-//
-//    if letter
-//    list = []
-//
-//    all_list.each do |item|
-//    if item[:name][0] == letter
-//    list << item
-//end
-//end
-//else
-//list = all_list
-//end
-//
-//fix_path(list)
+    let list = fixName(try getCategory( "actors-dropdown", document: document))
+
+    let allList = sortByName(list)
+
+    if !letter.isEmpty {
+      data = []
+
+      for item in allList {
+        let currentItem = item as! [String: Any]
+        let name = currentItem["name"] as! String
+
+        if name.hasPrefix(letter) {
+          data.append(item)
+        }
+      }
+    }
+    else {
+      data = allList
+    }
+
+    return fixPath(data)
+  }
+
+  public func getDirectors(_ document: Document, letter: String="") throws -> [Any] {
+    var data: [Any] = []
+
+    let list = fixName(try getCategory( "director-dropdown", document: document))
+
+    let allList = sortByName(list)
+
+    if !letter.isEmpty {
+      data = []
+
+      for item in allList {
+        let currentItem = item as! [String: Any]
+        let name = currentItem["name"] as! String
+
+        if name.hasPrefix(letter) {
+          data.append(item)
+        }
+      }
+    }
+    else {
+      data = allList
+    }
+
+    return fixPath(data)
+  }
+
+  func getCountries(_ document: Document) throws -> [Any] {
+    return fixPath(try getCategory("country-dropdown", document: document))
+  }
+
+  func getYears(_ document: Document) throws -> [Any] {
+    return fixPath(try getCategory("year-dropdown", document: document))
+  }
+
+  func getSeasons(_ path: String) throws -> [Any] {
+    return try getCategory("season", document: getMovieDocument(GidOnlineAPI.URL + path)!)
+  }
+
+  func getEpisodes(_ path: String) throws -> [Any] {
+    return try getCategory("episode", document: getMovieDocument(GidOnlineAPI.URL + path)!)
+  }
+
+  func getCategory(_ id: String, document: Document) throws -> [Any] {
+    var data: [Any] = []
+
+    let links = try document.select("select[id='" + id + "'] option")
+
+    for link: Element in links.array() {
+      let path = try link.attr("value")
+      let name = try link.text()
+
+      data.append(["path": path, "name": name])
+    }
 
     return data
   }
 
-  public func getDirectors(document: Document, letter: String="") throws -> [Any] {
-    var data: [Any] = []
+  func getMovieDocument(_ url: String, season: String="", episode: String="") throws -> Document? {
+    let gatewayUrl = try getGatewayUrl(fetchDocument(url)!)
 
-//    all_list = fix_name(get_category('director-dropdown', document))
-//
-//    all_list.sort_by! {|item| item[:name]}
-//
-//    if letter
-//    list = []
-//
-//    all_list.each do |item|
-//    if item[:name][0] == letter
-//    list << item
-//end
-//end
-//else
-//list = all_list
-//end
-//
-//fix_path(list)
+    var movieUrl: String!
 
-    return data
+    if !season.isEmpty {
+      movieUrl = "\(gatewayUrl)?season=\(season)&episode=\(episode)"
+    }
+    else {
+      movieUrl = gatewayUrl
+    }
+
+    print(movieUrl)
+
+    if movieUrl.contains("//www.youtube.com") {
+      movieUrl = movieUrl.replacingOccurrences(of: "//", with: "http://")
+    }
+
+    return try fetchDocument(movieUrl, headers: getHeaders(gatewayUrl))
   }
 
-//  func getCountries(document: Document) -> String {
-//    return fixPath(getCategory("country-dropdown", document))
-//  }
-//
-//  func getYears(document: Document) -> String {
-//    return fixPath(getCategory("year-dropdown", document))
-//  }
-//
-//  func getSeasons(document: Document) -> String {
-//    return getCategory("season", getMovieDocument(URL + path))
-//  }
-//
-//  func getSeasons(document: Document) -> String {
-//    return getCategory("episode", getMovieDocument(URL + path))
-//  }
+  func getGatewayUrl(_ document: Document) throws -> String {
+    var gatewayUrl: String!
 
-  func getHeaders(referer: String) -> [String: String] {
+    let frameBlock = try document.select("div[class=tray]")
+
+    var urls = try frameBlock.select("iframe[class=ifram]").attr("src")
+
+    if urls.characters.count > 0 {
+      gatewayUrl = urls
+    }
+    else {
+      let url = GidOnlineAPI.URL + "/trailer.php"
+
+      print(try document.select("head meta[id=meta]"))
+
+      let data = [
+        //"id_post": try document.select("head meta[id=meta]").select("@content").array()[0]
+        "id_post": "aaa"
+      ]
+
+      let response = httpRequest(url: url, data: data, method: "post")
+
+      let content = response.content
+
+      let document2 = try toDocument(content)
+
+      urls = try document2!.select("iframe[class='ifram']").attr("src")
+
+      if urls.trim().characters.count > 0 {
+        gatewayUrl = urls
+      }
+    }
+
+    return gatewayUrl
+  }
+
+  func fixName(_ items: [Any]) -> [Any] {
+    var newItems: [Any] = []
+
+    for item in items {
+      var currentItem = (item as! [String: Any])
+
+      let name = currentItem["name"] as! String
+
+      let names = name.components(separatedBy: " ")
+
+      var newName = ""
+      var suffix = ""
+      var delta = 0
+
+      if names[names.count-1] == "мл." {
+        delta = 1
+        suffix = names[names.count-1]
+
+        newName = names[names.count-2]
+      }
+      else {
+        newName = names[names.count-1]
+      }
+
+      if names.count > 1 {
+        newName += ","
+
+        for index in 0 ..< names.count-1-delta {
+          newName += " " + names[index]
+        }
+      }
+
+      if !suffix.isEmpty {
+        newName = newName + " " + suffix
+      }
+
+      currentItem["name"] = newName
+
+      newItems.append(currentItem)
+    }
+
+    return newItems
+  }
+
+  func fixPath(_ items: [Any]) -> [Any] {
+    var newItems: [Any] = []
+
+    for item in items {
+      var currentItem = (item as! [String: Any])
+
+      let path = currentItem["path"] as! String
+
+      let index1 = path.index(path.startIndex, offsetBy: GidOnlineAPI.URL.characters.count, limitedBy: path.endIndex)
+      let index2 = path.index(before: path.endIndex)
+
+      if index1 != nil {
+        currentItem["path"] = path[index1! ... index2]
+      }
+      else {
+        currentItem["path"] = path
+      }
+
+      newItems.append(currentItem)
+    }
+
+    return newItems
+  }
+
+  func getHeaders(_ referer: String) -> [String: String] {
     return [
       "User-Agent": USER_AGENT,
       "Referer": referer
     ]
+  }
+
+  func sortByName(_ list: [Any]) -> [Any] {
+    return list.sorted { element1, element2 in
+      let name1 = (element1 as! [String: Any])["name"] as! String
+      let name2 = (element2 as! [String: Any])["name"] as! String
+
+      return name1 < name2
+    }
   }
 
 }
--- Sources/String-extension.swift
+++ Sources/String-extension.swift
@@ -1,9 +1,9 @@
 extension String {
-  func find(_ sub: String) -> String.Index? {
+  public func find(_ sub: String) -> String.Index? {
     return self.range(of: sub)?.lowerBound
   }
 
-  func trim() -> String {
+  public func trim() -> String {
     return self.trimmingCharacters(in: .whitespaces)
   }
 
