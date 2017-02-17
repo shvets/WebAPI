e6d7aaaf943bbe728020ec914ca0be62dc04fb6c
Alexander Shvets <alexander.shvets@gmail.com>
2017-02-16 21:52:00 -0500

    update versions

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
   name: "WebAPI",
   dependencies: [
     .Package(url: "https://github.com/JustHTTP/Just.git", Version(0, 5, 7)),
-    .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", Version(3, 1, 3)),
-    .Package(url: "https://github.com/scinfu/SwiftSoup", Version(1, 1, 7))
+    .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", Version(3, 1, 4)),
+    .Package(url: "https://github.com/scinfu/SwiftSoup", Version(1, 2, 0))
   ]
 )
