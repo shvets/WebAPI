02811ce4adb3ca17eb7d5f75346f3c5fc95d9a35
Alexander Shvets <alexander.shvets@gmail.com>
2017-04-11 00:02:56 -0400

    add tests and code

--- Sources/KinoKongAPI.swift
+++ Sources/KinoKongAPI.swift
@@ -1,13 +1,65 @@
 import Foundation
+import SwiftSoup
 
 open class KinoKongAPI: HttpService {
   static let SiteUrl = "http://kinokong.cc"
   let UserAgent = "KinoKong User Agent"
 
   public func available() throws -> Bool {
-    let document = try fetchDocument(KinoKongAPI.SiteUrl, headers: getHeaders())
+    let document = try fetchDocument(KinoKongAPI.SiteUrl, headers: getHeaders(), encoding: .windowsCP1251)
 
     return try document!.select("div[id=container]").size() > 0
+  }
+
+  public func getAllMovies(page: Int=1) throws -> Items {
+    return try getMovies("/films/", page: page)
+  }
+
+  public func getMovies(_ path: String, page: Int=1) throws -> Items {
+    var data = [Any]()
+    var paginationData: Items = [:]
+
+    let pagePath = getPagePath(path: path, page: page)
+
+//    let document = try fetchDocument(KinoKongAPI.SiteUrl + pagePath, headers: getHeaders(), encoding: .windowsCP1251)
+//
+//    let items = try document!.select("div[class=owl-item] div")
+
+//    for item: Element in items.array() {
+//      let link = try item.select("a").get(0)
+//      let href = try link.attr("href")
+//
+//      let name = try link.attr("title")
+
+      //shadow_node = item.find('div[@class="main-sliders-shadow"]')
+//title_node = item.find('div[@class="main-sliders-title"]')
+//season_node = shadow_node.find('div/div[@class="main-sliders-season"]')
+//bg_node = shadow_node.find('div/span[@class="main-sliders-bg"]')
+//
+//href_link = bg_node.find('a[@class="main-sliders-play"]')
+//thumb_link = shadow_node.find('div/img')
+//
+//href = href_link.get('href')
+//href = href[len(self.URL):]
+//thumb = thumb_link.get('src')
+
+      //if thumb.find(self.URL) == -1:
+//thumb = self.URL + thumb
+//name = title_node.text_content()
+//
+//data.append({'path': href, 'thumb': thumb, 'name': name, 'isSerie': season_node is not None})
+//
+//    }
+
+    //pagination = self.extract_pagination_data(page_path, page=page)
+//
+//return {"items": data, "pagination": pagination["pagination"]}
+
+    //if items.size() > 0 {
+      //paginationData = try extractPaginationData(pagePath, selector: selector, page: page)
+    //}
+
+    return ["movies": data, "pagination": paginationData]
   }
 
   func getHeaders() -> [String: String] {
--- Tests/WebAPITests/KinoKongAPITests.swift
+++ Tests/WebAPITests/KinoKongAPITests.swift
@@ -8,10 +8,127 @@ import Unbox
 class KinoKongAPITests: XCTestCase {
   var subject = KinoKongAPI()
 
-//  func testGetAuthorsLetters() throws {
-//    let result = try subject.getAuthorsLetters()
+  func testGetAvailable() throws {
+    let result = try subject.available()
+
+    print(JsonConverter.prettified(result))
+  }
+
+  func testGetAllMovies() throws {
+    let result = try subject.getAllMovies()
+
+    print(JsonConverter.prettified(result))
+  }
+
+//  func testGetGroupedGenres() throws {
+//    let result = try subject.getGetGroupedGenres()
 //
 //    print(JsonConverter.prettified(result))
 //  }
 //
+//  func testGetUrls() throws {
+//    let path = "/26545-lovushka-dlya-privideniya-2015-smotret-online.html"
+//
+//    let result = try subject.getGetUrls(path)
+//
+//    print(JsonConverter.prettified(result))
+//  }
+//
+//  func testGetSeriePlaylistUrl() throws {
+//    let path = "/25213-rodoslovnaya-03-06-2016.html"
+//
+//    let result = try subject.getGetSeriePlaylistUrl(path)
+//
+//    print(JsonConverter.prettified(result))
+//  }
+//
+//  func testGetUrlMetadata() throws {
+//    let path = "/26545-lovushka-dlya-privideniya-2015-smotret-online.html"
+//
+//    let urls = try subject.getGetUrls(path)
+//
+//    let result = try subject.getGetUrlMetadata(urls)
+//
+//    print(JsonConverter.prettified(result))
+//  }
+//
+//  func testGetSearch() throws {
+//    let query = "красный"
+//
+//    let result = try subject.search(query)
+//
+//    print(JsonConverter.prettified(result))
+//  }
+//
+//  func testPaginationInAllMovies() throws {
+//    let result1 = try subject.getAllMovies(page: 1)
+//
+//    let pagination1 = result1["pagination"] as! [String: Any]
+//
+//    XCTAssertTrue(pagination1["has_next"] as! Bool)
+//    XCTAssertFalse(pagination1["has_previous"] as! Bool)
+//    XCTAssertEqual(pagination1["page"] as! Int, 1)
+//
+//    let result2 = try subject.getAllMovies(page: 2)
+//
+//    let pagination2 = result2["pagination"] as! [String: Any]
+//
+//    print(pagination2)
+//
+//    XCTAssertTrue(pagination2["has_next"] as! Bool)
+//    XCTAssertTrue(pagination2["has_previous"] as! Bool)
+//    XCTAssertEqual(pagination2["page"] as! Int, 2)
+//  }
+//
+//  func testPaginationInMoviesByRating() throws {
+//    let result1 = try subject.getMoviesByRating(page: 1)
+//
+//    let pagination1 = result1["pagination"] as! [String: Any]
+//
+//    XCTAssertTrue(pagination1["has_next"] as! Bool)
+//    XCTAssertFalse(pagination1["has_previous"] as! Bool)
+//    XCTAssertEqual(pagination1["page"] as! Int, 1)
+//
+//    let result2 = try subject.getMoviesByRating(page: 2)
+//
+//    let pagination2 = result2["pagination"] as! [String: Any]
+//
+//    print(pagination2)
+//
+//    XCTAssertTrue(pagination2["has_next"] as! Bool)
+//    XCTAssertTrue(pagination2["has_previous"] as! Bool)
+//    XCTAssertEqual(pagination2["page"] as! Int, 2)
+//  }
+//
+//  func testGetSerieInfo() throws {
+//    let series = subject.getAllSeries()["items"]
+//
+//    let path = "/28206-v-obezd-2015-07-06-2016.html"
+//
+//    let playlistUrl = subject.getSeriePlaylistUrl(path)
+//    let result = try subject.getSerieInfo(playlistUrl)
+//
+//    print(JsonConverter.prettified(result))
+//  }
+//
+//  func testGetMoviesByRating() throws {
+//    let result = try subject.getMoviesByRating()
+//
+//    print(JsonConverter.prettified(result))
+//  }
+//
+//  func testGetTags() throws {
+//    let result = try subject.getTags()
+//
+//    print(JsonConverter.prettified(result))
+//  }
+//
+//  func testGetSoundtracks() throws {
+//    let path = "/15479-smotret-dedpul-2016-smotet-online.html"
+//
+//    let playlistUrl = subject.getSeriePlaylistUrl(path)
+//    let result = subject.getSerieInfo(playlistUrl)
+//
+//    print(JsonConverter.prettified(result))
+//  }
 }
