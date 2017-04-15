6b6df3ed16ba4052cba999c5512771a3aeb65002
Alexander Shvets <alexander.shvets@gmail.com>
2017-04-15 16:37:53 -0400

    update appletv code

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 let package = Package(
   name: "WebAPI",
   dependencies: [
-    .Package(url: "https://github.com/JustHTTP/Just.git", Version(0, 5, 7)),
+    .Package(url: "https://github.com/JustHTTP/Just.git", Version(0, 6, 0)),
     .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", Version(3, 1, 4)),
-    .Package(url: "https://github.com/scinfu/SwiftSoup", Version(1, 2, 6)),
+    .Package(url: "https://github.com/scinfu/SwiftSoup", Version(1, 3, 0)),
     .Package(url: "https://github.com/JohnSundell/Wrap.git", Version(2, 1, 0)),
-    .Package(url: "https://github.com/JohnSundell/Unbox.git", Version(2, 3, 1))
+    .Package(url: "https://github.com/JohnSundell/Unbox.git", Version(2, 4, 0))
   ]
 )
