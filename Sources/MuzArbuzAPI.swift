d978f26477b66972df05e1ae4d2467301d9e737d
Alexander Shvets <alexander.shvets@gmail.com>
2017-06-04 21:03:47 -0400

    add support for muzarbuz

--- Sources/MuzArbuzAPI.swift
+++ Sources/MuzArbuzAPI.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftyJSON
 
 open class MuzArbuzAPI: HttpService {
   static let SiteUrl = "https://muzarbuz.com"
@@ -8,9 +9,117 @@ open class MuzArbuzAPI: HttpService {
   let ValidParameters = ["album", "artists", "collection__id", "parent__id", "genre__in"]
 
   let CyrillicLetters = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С",
-      "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"]
+                         "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"]
 
   let LatinLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
-    "T", "U", "V", "W", "X", "Y", "Z"]
+                      "T", "U", "V", "W", "X", "Y", "Z"]
+
+  func getAlbums(_ params: [String: String]=[:]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/album", params: params as [String : AnyObject])
+
+    print(url)
+
+    return try apiRequest(url)
+  }
+
+  func getTracks(_ params: [String: String]=[:]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/audio_track", params: params as [String : AnyObject])
+
+    return try apiRequest(url)
+  }
+
+  func getArtists(_ params: [String: String]=[:]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/artist", params: params as [String : AnyObject])
+
+    return try apiRequest(url)
+  }
+
+  func getArtistAnnotated(_ params: [String: String]=[:]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/artist_annotated", params: params as [String : AnyObject])
+
+    return try apiRequest(url)
+  }
+
+  func getCollections(_ params: [String: String]=[:]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/collection", params: params as [String : AnyObject])
+
+    return try apiRequest(url)
+  }
+
+  func getGenres(_ params: [String: String]=[:]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/genre", params: params as [String : AnyObject])
+
+    return try apiRequest(url)
+  }
+
+  func search(query: String) throws -> [String: Any] {
+    var params = [String: String]()
+    params["q"] = query.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
+
+    return [
+      "collection": try searchCollection(params: params),
+      "artist_annotated": try searchArtistAnnotated(params: params),
+      "album": try searchAlbum(params: params),
+      "audio_track": try searchAudioTrack(params: params)
+    ]
+  }
+
+  func searchCollection(params: [String: String]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/collection/search/", params: params as [String: AnyObject])
+
+    return try apiRequest(url)
+  }
+
+  func searchArtistAnnotated(params: [String: String]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/artist_annotated/search/", params: params as [String: AnyObject])
+
+    return try apiRequest(url)
+  }
+
+  func searchAlbum(params: [String: String]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/album/search/", params: params as [String: AnyObject])
+
+    return try apiRequest(url)
+  }
+
+  func searchAudioTrack(params: [String: String]) throws -> JSON {
+    let url = buildUrl(path: MuzArbuzAPI.ApiUrl + "/audio_track/search/", params: params as [String: AnyObject])
+
+    return try apiRequest(url)
+  }
+
+//def filter_request_params(self, params):
+//return dict((key, value) for key, value in params.iteritems() if key in self.VALID_PARAMETERS)
+//
+
+  func add_pagination_to_response(response: [String: String], page: Int, per_page: Int) {
+  //  pages = float(response['meta']['total_count']) / float(per_page)
+  //
+  //if pages > int(pages):
+  //pages = int(pages) + 1
+  //else:
+  //pages = int(pages)
+  //
+  //response['data'] = {'pagination': {
+  //  'page': page,
+  //  'pages': pages,
+  //  'has_next': page < pages,
+  //  'has_previous': page > 1
+  //}}
+  }
+
+  func apiRequest(_ url: String) throws -> JSON {
+    var headers: [String: String] = [:]
+
+    headers["User-agent"] = UserAgent
+    headers["Content-Type"] = "application/json"
+
+    if let data = fetchData(url, headers: headers) {
+      return JSON(data: data)
+    }
+    else {
+      return JSON.null
+    }
+  }
 
 }
--- Tests/WebAPITests/MuzArbuzAPITests.swift
+++ Tests/WebAPITests/MuzArbuzAPITests.swift
@@ -8,10 +8,78 @@ import Unbox
 class MuzArbuzAPITests: XCTestCase {
   var subject = MuzArbuzAPI()
 
-//  func testGetAuthorsLetters() throws {
-//    let result = try subject.getAuthorsLetters()
-//
-//    print(JsonConverter.prettified(result))
-//  }
-//
+  func testGetAlbums() throws {
+    let result = try subject.getAlbums(["limit": "20"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetAlbumContainer() throws {
+    let result = try subject.getAlbums(["parent_id": "14485"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetAlbumsByYearRange() throws {
+    let result = try subject.getAlbums(["year__gte": "2014", "year__lte": "2015"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetArtistTracks() throws {
+    let result = try subject.getTracks(["artists": "1543"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetAlbumTracks() throws {
+    let result = try subject.getTracks(["album": "14486"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetCollectionTracks() throws {
+    let result = try subject.getTracks(["collection__id": "115"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetArtists() throws {
+    let result = try subject.getArtists()
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetArtistAnnotated() throws {
+    let result = try subject.getArtistAnnotated(["title__istartswith": "В"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetCollections() throws {
+    let result = try subject.getCollections(["limit": "25"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetGenres() throws {
+    let result = try subject.getGenres()
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testGetAlbumsByGenre() throws {
+    let result = try subject.getAlbums(["genre__in": "1"])
+
+    print(JsonConverter.prettified(result["objects"]))
+  }
+
+  func testSearch() throws {
+    let result = try subject.search(query: "макаревич")
+
+    print(result)
+  }
+
 }
+
+
