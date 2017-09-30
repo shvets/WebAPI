b229a621c289a9c7f859e2adef4162e8da9c23c7
Alexander Shvets <alexander.shvets@gmail.com>
2017-09-30 11:11:06 -0400

    bump SwiftSoup version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/Alamofire/Alamofire", Version(4, 5, 1)),
     .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", Version(3, 1, 4)),
-    .Package(url: "https://github.com/scinfu/SwiftSoup", Version(1, 5, 0))
+    .Package(url: "https://github.com/scinfu/SwiftSoup", Version(1, 5, 1))
   ]
 )
